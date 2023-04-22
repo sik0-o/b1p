e12a7b00c5d10dac9e823aebbe99373ff4a0ab52
sikOo <siko.sik.1@gmail.com>
2023-04-22 22:17:11 +0300

    add proxy list

---
+++ proxy/list.go
@@ -0,0 +1,75 @@
+package proxy
+
+import "sync"
+
+type List struct {
+	sync.RWMutex
+	list map[string]struct{}
+}
+
+func NewList(proxies []string) *List {
+	pl := &List{
+		list: make(map[string]struct{}),
+	}
+
+	pl.Lock()
+	for _, proxy := range proxies {
+		pl.list[proxy] = struct{}{}
+	}
+	pl.Unlock()
+
+	return pl
+}
+
+func (pl *List) Pick() string {
+	pl.Lock()
+	defer pl.Unlock()
+
+	for p, _ := range pl.list {
+		delete(pl.list, p)
+		return p
+	}
+
+	return ""
+}
+
+func (pl *List) Fetch() map[string]struct{} {
+	pl.RLock()
+	defer pl.RUnlock()
+
+	return pl.list
+}
+
+func (pl *List) Add(proxy string) {
+	pl.Lock()
+	defer pl.Unlock()
+
+	pl.list[proxy] = struct{}{}
+}
+
+func (pl *List) Remove(proxy string) {
+	pl.Lock()
+	defer pl.Unlock()
+
+	delete(pl.list, proxy)
+}
+
+func (pl *List) Has(proxy string) bool {
+	pl.RLock()
+	defer pl.RUnlock()
+
+	_, ok := pl.list[proxy]
+
+	return ok
+}
+
+func (pl *List) Get() string {
+	pl.RLock()
+	defer pl.RUnlock()
+
+	for proxy, _ := range pl.list {
+		return proxy
+	}
+
+	return ""
+}
